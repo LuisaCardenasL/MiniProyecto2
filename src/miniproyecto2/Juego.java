63e0c215722cc05de9689c01867442ab315fc636
1823494 Luisa Cardenas <cardenas.luisa@correounivalle.edu.co>
2022-06-30 22:27:22 -0500

    Creacion clase Juego

---
+++ src/miniproyecto2/Juego.java
@@ -0,0 +1,40 @@
+package miniproyecto2;
+
+import java.util.Random;
+import java.util.random.RandomGenerator;
+
+/**
+ *
+ * @author Lu
+ */
+public class Juego {
+    int mat [][] = new int [4][4];
+    Random random;
+    
+    private void cartasAleatorio(){
+        int acumulador = 0;
+        for (int i = 0; i < 4; i++)
+            for (int j = 0; j < 4; j++){
+                mat[i][j] = 0;
+        }
+        
+        for (int i = 0; i < 4; i++)
+            for (int j = 0; j < 4; j++){
+                mat[i][j] = random.nextInt(8)+1;
+                
+                do{
+                    acumulador = 0;
+                    for (int k = 0; k < 4; k++)
+                        for (int l = 0; l < 4; l++){
+                                if(mat[i][j] == mat[k][l]){
+                                    acumulador +=1;
+                                }
+                            }
+                if(acumulador == 3){
+                    mat[i][j] = random.nextInt(8)+1;
+                }
+                }while (acumulador == 0);
+            }
+        }
+}
+
